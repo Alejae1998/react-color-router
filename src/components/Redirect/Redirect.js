532e11fd7f91be7c3b528e49ab4606e332d0cbc0
Alejae1998 <alejagr1998@gmail.com>
2023-01-24 11:28:45 -0800

    adding my rgbs and the active class

--- src/components/Redirect/Redirect.js
+++ src/components/Redirect/Redirect.js
@@ -1,1 +1,21 @@
 import { NavLink, useParams } from 'react-router-dom';
+import './Redirect.css';
+
+export default function Redirect() {
+  const { r, g, b } = useParams();
+  const color = `rgb ( ${r}, ${g}, ${b} )`;
+
+  return (
+    <div className="redirect" style={{ color: color }}>
+      <NavLink to="/rgb/160/230/255" className="colors">
+        Winter Wizard
+      </NavLink>
+      <NavLink to="/rgb/255/166/220" className="colors">
+        Lavender Rose
+      </NavLink>
+      <NavLink to="/rgb/255/0/125" className="colors">
+        Winter Sky
+      </NavLink>
+    </div>
+  );
+}
