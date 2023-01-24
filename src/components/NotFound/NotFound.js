53eb44c2aa3b9b260fccd97d3efa29e36e83dac6
Alejae1998 <alejagr1998@gmail.com>
2023-01-24 13:47:26 -0800

    finishing the 404

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -5,17 +5,14 @@ export default function NotFound() {
   return (
     <span>
       <div className=".container">
-        <div className=".col-lg-12">
-          <div className=".bg-img">
-            <h1>404</h1>
-            <h2>Oops, the color that you are looking for does not exist</h2>
-            <p>
-              You may want to go<Link to="/">home</Link>
-            </p>
-          </div>
-        </div>
+        <h1>404</h1>
+        <h2>Oops, the color that you are looking for does not exist in our palette</h2>
+        <p>
+          You may want to go<Link to="/">home</Link>
+        </p>
       </div>
-      <div className=".bg-img"></div>
+
+      <div className="img"></div>
     </span>
   );
 }
